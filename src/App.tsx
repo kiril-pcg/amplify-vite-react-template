e5fad6d0b19d99d22e8aa5f8594a444d8563b58b
trencevkiril <trenchev.kiril@gmail.com>
2024-10-03 14:10:18 +0200

    added shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Industries from "./pages/Industries/Industries";
@@ -22,39 +21,3 @@ function App() {
 }
 
 export default App;
-
-// const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-// useEffect(() => {
-//   client.models.Todo.observeQuery().subscribe({
-//     next: (data) => setTodos([...data.items]),
-//   });
-// }, []);
-
-// function createTodo() {
-//   client.models.Todo.create({ content: window.prompt("Todo content") });
-// }
-
-// function deleteTodo(id: string) {
-//   client.models.Todo.delete({ id });
-// }
-
-        // <main>
-        //   <h1>My todos</h1>
-        //   <button onClick={createTodo}>+ new</button>
-        //   <ul>
-        //     {todos.map((todo) => (
-        //       <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
-        //         {todo.content}
-        //       </li>
-        //     ))}
-        //   </ul>
-        //   <div>
-        //     🥳 App successfully hosted. Try creating a new todo.
-        //     <br />
-        //     <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-        //       Review next step of this tutorial.
-        //     </a>
-        //   </div>
-        //   <button onClick={signOut}>Sign out</button>
-        // </main>
