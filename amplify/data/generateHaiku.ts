2b717946ed6d037ef24aa0852b3d8066cf352e50
trencevkiril <trenchev.kiril@gmail.com>
2024-10-07 15:29:30 +0200

    modified lambda

--- amplify/data/generateHaiku.ts
+++ amplify/data/generateHaiku.ts
@@ -1,9 +1,41 @@
 import type { Schema } from "./resource";
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from "@aws-sdk/client-bedrock-runtime";
+
+const client = new BedrockRuntimeClient();
 
 export const handler: Schema["generateHaiku"]["functionHandler"] = async (
   event,
   context
 ) => {
   const prompt = event.arguments.prompt;
-  return `The prompt is: ${prompt}`
+  const modelId = process.env.MODEL_ID;
+
+  const payload = {
+    inputText: prompt,
+    textGenerationConfig: {
+      maxTokenCount: 4096,
+      stopSequences: [],
+      temperature: 0,
+      topP: 1,
+    },
+  };
+
+
+  const command = new InvokeModelCommand({
+    contentType: "application/json",
+    body: JSON.stringify(payload),
+    modelId,
+  });
+
+  const response = await client.send(command);
+
+  const decodedResponseBody = new TextDecoder().decode(response.body);
+  const responseBody = JSON.parse(decodedResponseBody);
+
+  console.log(responseBody.results[0].outputText); 
+
+  return responseBody.results[0].outputText;
 };
