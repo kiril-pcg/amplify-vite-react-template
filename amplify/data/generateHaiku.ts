694da29a60ab008a61a896fe76c39f0905d4f2a2
trencevkiril <trenchev.kiril@gmail.com>
2024-10-07 14:29:08 +0200

    Testing lambda

--- amplify/data/generateHaiku.ts
+++ amplify/data/generateHaiku.ts
@@ -1,42 +1,9 @@
 import type { Schema } from "./resource";
-import {
-  BedrockRuntimeClient,
-  InvokeModelCommand,
-} from "@aws-sdk/client-bedrock-runtime";
-
-// initialize bedrock runtime client
-const client = new BedrockRuntimeClient();
 
 export const handler: Schema["generateHaiku"]["functionHandler"] = async (
   event,
   context
 ) => {
   const prompt = event.arguments.prompt;
-  const modelId = process.env.MODEL_ID;
-
-  const payload = {
-    inputText: prompt,
-    textGenerationConfig: {
-      maxTokenCount: 4096,
-      stopSequences: [],
-      temperature: 0,
-      topP: 1,
-    },
-  };
-
-
-  const command = new InvokeModelCommand({
-    contentType: "application/json",
-    body: JSON.stringify(payload),
-    modelId,
-  });
-
-  const response = await client.send(command);
-
-  const decodedResponseBody = new TextDecoder().decode(response.body);
-  const responseBody = JSON.parse(decodedResponseBody);
-
-  console.log(responseBody.results[0].outputText); 
-
-  return responseBody.results[0].outputText;
+  return `The prompt is: ${prompt}`
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,55 +1,202 @@
-import { FormEvent, useState } from "react";
-
-import { generateClient } from "aws-amplify/api";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Textarea } from "@/components/ui/textarea";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { client } from "../../utils/utils";
 import { Schema } from "../../../amplify/data/resource";
 
-const client = generateClient<Schema>();
+const formSchema = z.object({
+  bio: z
+    .string()
+    .min(10, {
+      message: "Bio must be at least 10 characters.",
+    })
+    .max(500, {
+      message: "Bio must not be longer than 500 characters.",
+    }),
+  industry: z.string({
+    required_error: "Please select an industry.",
+  }),
+  prompt: z
+    .string()
+    .min(10, {
+      message: "Prompt must be at least 10 characters.",
+    }),
+});
 
 export default function Home() {
-  const [prompt, setPrompt] = useState<string>("");
-  const [answer, setAnswer] = useState<string | null>(null);
+  const [industries, setIndustries] = useState<
+    Array<Schema["Industries"]["type"]>
+  >([]);
 
-  const sendPrompt = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const { data, errors } = await client.queries.generateHaiku({
-      prompt,
+  useEffect(() => {
+    client.models.Industries.observeQuery().subscribe({
+      next: (data) => setIndustries([...data.items]),
     });
+  }, []);
 
-    if (!errors) {
-      setAnswer(data);
-      setPrompt("");
-    } else {
-      console.log(errors);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      bio: "",
+      industry: "",
+      prompt: "",
+    },
+  });
+
+  const watchIndustry = form.watch("industry");
+
+  // Update the prompt field when a new industry is selected
+  useEffect(() => {
+    const selectedIndustry = industries.find(
+      (i) => i.industryName === watchIndustry
+    );
+    if (selectedIndustry) {
+      form.setValue("prompt", selectedIndustry.prompt || "");
     }
-  };
+  }, [watchIndustry, form, industries]);
+
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log("data: ", data);
+    const response = await client.queries.generateHaiku({prompt: data.prompt})
+    console.log(response);
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 dark:text-white">
-      <div>
-        <h1 className="text-3xl font-bold text-center mb-4">Haiku Generator</h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-2xl p-6 bg-white rounded-lg shadow-md">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="bio"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Bio</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Tell us about yourself"
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
 
-        <form className="mb-4 self-center max-w-[500px]" onSubmit={sendPrompt}>
-          <input
-            className="text-black p-2 w-full"
-            placeholder="Enter a prompt..."
-            name="prompt"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-        </form>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <button 
-            type="submit" 
-            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-          >
-            Generate Haiku
-          </button>
+            <FormField
+              control={form.control}
+              name="industry"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Industry</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          role="combobox"
+                          className={cn(
+                            "w-full justify-between",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value
+                            ? industries.find(
+                                (industry) => industry.industryName === field.value
+                              )?.industryName
+                            : "Select industry"}
+                          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-full p-0">
+                      <Command>
+                        <CommandInput placeholder="Search industry..." />
+                        <CommandList>
+                          <CommandEmpty>No industry found.</CommandEmpty>
+                          <CommandGroup>
+                            {industries.map((industry) => (
+                              <CommandItem
+                                value={industry.industryName || ""}
+                                key={industry.id}
+                                onSelect={() => {
+                                  form.setValue("industry", industry.industryName || "");
+                                }}
+                              >
+                                <Check
+                                  className={cn(
+                                    "mr-2 h-4 w-4",
+                                    industry.industryName === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                                {industry.industryName}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <div className="text-center">
-          <pre>{answer}</pre>
-        </div>
+            <FormField
+              control={form.control}
+              name="prompt"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Prompt</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Industry-specific prompt"
+                      className="min-h-[250px] max-h-[28vw]"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-center">
+              <Button type="submit" className="w-full">
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Form>
       </div>
-    </main>
+    </div>
   );
 }
